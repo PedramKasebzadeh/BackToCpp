6c46a9a30b6d7e53c6f4fc49bf8721a2a95a7718
Pedram Kasebzadeh <pedram.kasebzadeh@gmail.com>
2020-09-09 15:43:27 +0200

    IOSTREAM

---
+++ IntroToCpp/My own.cpp
@@ -0,0 +1,20 @@
+//
+//  My own.cpp
+//  IntroToCpp
+//
+//  Created by Pedram  on 2020-09-09.
+//  Copyright © 2020 Pedram . All rights reserved.
+//
+# include <iostream>
+using namespace std;
+
+int main(){
+    string name;
+    string food;
+    cout << "What is your Name? \n";
+    cin>> name;
+    cout << "What is your favourate food? \n";
+    cin>> food;
+    cout << name <<" likes " << food << "." <<endl;
+    return 0;
+}
